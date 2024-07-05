f9dc9b922d3353e2ce462d23282c7ad26b2df437
Arjun <arjun8107@gmail.com>
2024-07-06 00:11:35 +0530

    ????

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,17 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import legacy from "vite-plugin-legacy-swc";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import vConsolePlugin from "vite-plugin-simple-vconsole";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react({}),
     TanStackRouterVite(),
+    vConsolePlugin({
+      enable: true,
+      theme: "dark",
+    }),
     legacy({
       targets: ["chrome 76"],
     }),
